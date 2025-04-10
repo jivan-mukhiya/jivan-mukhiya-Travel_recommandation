09abffb46336ec27f39993a357a84d6f29f0137f
jivan-mukhiya <your-email@example.com>
2025-04-10 18:41:22 +0545

    first commit

---
+++ src/main/java/com/texas/traveldestinationrecommendation/TraveldestinationrecommendationApplication.java
@@ -0,0 +1,13 @@
+package com.texas.traveldestinationrecommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraveldestinationrecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraveldestinationrecommendationApplication.class, args);
+	}
+
+}
